6bd4a0cae3184aee03c6730df16878053950a517
Akram Izzeldin <akram.izz.kh@gmail.com>
2019-01-01 17:08:29 +0200

    visitor pattern implementation in AST classes

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -33,8 +33,15 @@ public class GenerateAst {
 		writer.println();
 		writer.println("abstract class " + baseName + " {");
 		
+		defineVisitor(writer, baseName, types);
+
+		// Abstart accept method for visitor pattern
+		
+		writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+		
 		// Write each type as a static class in [baseName] class
 		for (String type : types) {
+			writer.println();
 			String className = type.split(":")[0].trim();
 			String fields = type.split(":")[1].trim();
 			defineType(writer, baseName, className, fields);
@@ -42,6 +49,19 @@ public class GenerateAst {
 
 		writer.println("}");
 		writer.close();
+	}
+
+	// Defines visitor interface
+	private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+		writer.println("    interface Visitor<R> {");
+		for (String type : types) {
+			String typeName = type.split(":")[0].trim();
+			writer.println("        R visit" + typeName + baseName + "("
+				+ typeName + " " + baseName.toLowerCase() + ");");
+		}
+
+		writer.println("    }");
+		writer.println();
 	}
 
 	private static void defineType(
@@ -55,9 +75,11 @@ public class GenerateAst {
 
 		// Extract each field's name
 		String[] fieldsList = fields.split(", ");
-		
-		// fields value assignment
+
+		// Fields value assignment
 		for (String field : fieldsList) {
+			// Field contains fieldType and fieldName in a string
+			// seperated by a space
 			String name = field.split(" ")[1];
 			writer.println("            this." + name + " = " + name + ";");
 		}
@@ -65,6 +87,11 @@ public class GenerateAst {
 		writer.println("        }");
 		writer.println();
 		
+		writer.println("        <R> R accept(Visitor<R> visitor) {");
+		writer.println("            return visitor.visit" + className + baseName + "(this);");
+		writer.println("        }");
+		writer.println();
+
 		// Class fields definition
 		for (String field : fieldsList) {
 			writer.println("        final " + field + ";");
