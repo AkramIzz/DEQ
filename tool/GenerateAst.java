83890d833834635febf5ab0e17163376fd133c65
Akram Izzeldin <akram.izz.kh@gmail.com>
2019-01-01 14:17:37 +0200

    automate AST classes definition writing

---
+++ tool/GenerateAst.java
@@ -0,0 +1,76 @@
+package com.interpreter.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			System.err.println("Usage: generate_ast <output directory>");
+			System.exit(1);
+		}
+		String outputDir = args[0];
+		defineAst(outputDir, "Expr", Arrays.asList(
+			// ClassName : Fields
+			"Binary   : Expr left, Token operator, Expr right",
+			"Grouping : Expr expression",
+			"Literal  : Object value",
+			"Unary    : Token operator, Expr right"
+		));
+	}
+
+	private static void defineAst(
+			String outputDir, String baseName, List<String> types) 
+			throws IOException {
+		String path = outputDir + "/" + baseName + ".java";
+		PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+		writer.println("package com.interpreter;");
+		writer.println();
+		writer.println("import java.util.List;");
+		writer.println();
+		writer.println("abstract class " + baseName + " {");
+		
+		// Write each type as a static class in [baseName] class
+		for (String type : types) {
+			String className = type.split(":")[0].trim();
+			String fields = type.split(":")[1].trim();
+			defineType(writer, baseName, className, fields);
+		}
+
+		writer.println("}");
+		writer.close();
+	}
+
+	private static void defineType(
+			PrintWriter writer, String baseName,
+			String className, String fields) {
+		// defintion of type class
+		writer.println("    static class " + className + " extends " + baseName + " {");
+		
+		// Constructor
+		writer.println("        " + className + "(" + fields + ") {");
+
+		// Extract each field's name
+		String[] fieldsList = fields.split(", ");
+		
+		// fields value assignment
+		for (String field : fieldsList) {
+			String name = field.split(" ")[1];
+			writer.println("            this." + name + " = " + name + ";");
+		}
+		// End of constructor
+		writer.println("        }");
+		writer.println();
+		
+		// Class fields definition
+		for (String field : fieldsList) {
+			writer.println("        final " + field + ";");
+		}
+
+		// End of class
+		writer.println("    }");
+	}
+}
