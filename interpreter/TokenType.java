5f64be09b08e761dcede29e042e9ba29d25b45cf
Akram Izzeldin <akram.izz.kh@gmail.com>
2019-01-03 21:18:15 +0200

    Added token types to support ternary operator and integrated them into the scanner

--- interpreter/TokenType.java
+++ interpreter/TokenType.java
@@ -2,8 +2,9 @@ package com.interpreter;
 
 enum TokenType {
 	// Single-character tokens
-	LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-	COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+	LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE,
+	RIGHT_BRACE, COMMA, DOT, MINUS, PLUS,
+	SEMICOLON, SLASH, STAR, QUESTION_MARK, COLON,
 
 	// One or two character tokens
 	BANG, BANG_EQUAL,
